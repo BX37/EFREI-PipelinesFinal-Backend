bd7bea1fcc7a47ab67931534994aa85fc759dad6
BX37 <123464815+BX37@users.noreply.github.com>
2026-04-29 12:10:24 +0200

    change

--- migration.js
+++ migration.js
@@ -1,7 +1,6 @@
 import { Sequelize, DataTypes } from 'sequelize';
-import dotenv from 'dotenv';
-
-dotenv.config();
+import { config } from 'dotenv';
+config({ override: true });
 
 const DB_NAME = process.env.DB_NAME;
 const DB_USER = process.env.DB_USER;
